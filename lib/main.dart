5b202cad9cc294131074548c4661ac19897a7eb2
bagasayn <bagasayn@gmil.com>
2020-03-25 10:26:38 +0300

    Home version

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
 
-import 'question.dart';
-import 'package:quizzler/question.dart';
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -39,14 +39,6 @@ class _QuizPageState extends State<QuizPage> {
 //    ),
   ];
 
-  List<Question> questionList = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
-
   int number = 0;
 
   @override
@@ -61,7 +53,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionList[number].questionText,
+                quizBrain.questionList[number].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -86,9 +78,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //the user picked true,
-                bool correctAnswers = questionList[number].questionAnswer;
-
-                if (correctAnswers == true) {
+                if (quizBrain.questionList[number].questionAnswer == true) {
                   scoreKeeper.add(Icon(
                     Icons.check,
                     color: Colors.green,
@@ -100,7 +90,7 @@ class _QuizPageState extends State<QuizPage> {
                   ));
                 setState(
                   () {
-                    if (number != 2) {
+                    if (number != 12) {
                       number++;
                     } else
                       number = 0;
@@ -125,9 +115,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswers = questionList[number].questionAnswer;
-
-                if (correctAnswers == false) {
+                if (quizBrain.questionList[number].questionAnswer == false) {
                   scoreKeeper.add(Icon(
                     Icons.check,
                     color: Colors.green,
@@ -139,7 +127,7 @@ class _QuizPageState extends State<QuizPage> {
                   ));
                 setState(
                   () {
-                    if (number != 2) {
+                    if (number != 12) {
                       number++;
                     } else
                       number = 0;
